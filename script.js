5268eddca111532a95ea3cecb274ce1ed83f25be
JoseGonzalezHELP <jogonzalezcr@guanajuato.gob.mx>
2025-08-14 12:04:04 -0600

    Update script.js

--- script.js
+++ script.js
@@ -15,14 +15,6 @@ firebase.initializeApp(firebaseConfig);
 const database = firebase.database();
 
 // Prueba de conexión
-database.ref('.info/connected').on('value', (snapshot) => {
-  if (snapshot.val() === true) {
-    console.log("✅ Conectado a Firebase");
-  } else {
-    console.error("❌ Error de conexión a Firebase");
-    alert("Error de conexión. Recarga la página o revisa tu internet.");
-  }
-});
 
 // Referencias a la base de datos
 const inventoryRef = database.ref('inventory');
